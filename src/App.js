52a96c2d71cae6141bf8277a255b66348de5a83f
mayank <mayanksingh99001@gmail.com>
2023-07-25 13:15:14 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button1 from './components/Button1';
+import './App.css';
+import Card1 from './components/Card1'
+
+function App() {
+  return (
+    <>
+      {/* <div className="App"><Button1/></div> */}
+      <Card1/>
+      
+    </>
+  );
+}
+
+export default App;
